29afa0e25be3f2be901fd2f0053eac5f81e9beff
Chukwuma Ibezim <123107253+cibezim@users.noreply.github.com>
2024-04-22 14:02:51 -0400

    feat: adds back end folder with food-differently folder inside. Needed Spring dependencies added  (#1)
    * feat: adds food-diffrently spring folder, frontend and backend folders
    
    * fix: removes .DS_Store

---
+++ backend/food-differently/src/test/java/com/fooddifferently/fooddifferently/FoodDifferentlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddifferently.fooddifferently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDifferentlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
